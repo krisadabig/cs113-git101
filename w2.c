106729d51676f9c9356d0fef80c143c2dc61f2d1
big <krisada.m@ku.th>
2020-02-12 22:10:59 +0700

    week3 gets, fgets, getchar, atoi, atof, if-else, boolean, loop

---
+++ w2.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+ char name[20];
+int main()
+{
+    //printf("Enter name :");
+    //gets(name);
+    //fgets(name, 20 stdin);
+    char number[5];
+    //fgets(number, 5, stdin);
+    //int i = atoi(number);
+    //printf("number = %s, i = %d\n", number, i);
+    //char a = getchar();
+    //getchar();
+    //char b = getchar();
+    //long 1 = atoi(number);
+    // double d = atof(number);
+
+    //puts(number);
+    //putchar(100); putchar('a');
+    // int found = 0;
+    // float isExit = 0.0;
+    // char isOnline = '\0';
+
+    // fgets(number, 5, stdin);
+    // int i = atoi(number);
+    // if (i >= 0 && i <=10){
+    //     printf("if = %d", i >= 0 && i <= 10);
+    // }else if (i <= 50 || !found){
+    //     printf("else if");
+    // }else {
+    //     printf("else");
+    // }
+
+    fgets(number, 5, stdin);
+    int i = atoi(number);
+    // int count = 0;
+    // while (count < i){
+    //     printf("%d\n", count);
+    //     count++;
+    // }
+    // for (int j = 0; j <i; j++){
+    //     printf("%d\n", j);
+    // }
+    // while (1);{
+    //     infinite loop
+    // }
+}
