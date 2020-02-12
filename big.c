f23294d31fd77b3da05a6149e51ebb7b1586e0f1
big <krisada.m@ku.th>
2020-02-12 22:04:47 +0700

    week2 basic, data types

---
+++ big.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define bb "big"
+char r = '\0';
+int main()
+{
+    //printf("Hello WOrld!");
+    unsigned long long a = 4000000000;
+    char b = 'a';
+    char c = 97;
+    float f = 0.1245;
+    double d = 20.12222;
+    //printf("%c %d %c %d", b,b,c,c);
+    float e ;
+    printf("%f ", e+1000);
+}
